6438d2b86e18ab882cd3460a23a1cfd25e90babf
Myles181 <cyrile450@gmail.com>
2024-01-28 07:06:50 -0800

    apis

---
+++ main/routes.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+""" MODEL ROUTES"""
+
+@app.route('/cv-analyser')
+def cv_analyser():
+    pass
+
+@app.route('/email-writer')
+def email_writer():
+    pass
+
+@app.route('/followback_email')
+def followback_email():
+    pass
+
+@app.route('/job-recommendation')
+def job_recommendation():
+    pass
+
+""" Notifications """
+
+
+if __name__ == "__main__":
+    app.run()
