fbac841e5820035e0ee6b9e1e4ea98a84ca71226
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-02-08 16:14:47 -0800

    Update user.model.js

--- users/src/database/models/user.model.js
+++ users/src/database/models/user.model.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     // Application Requirement Info
     verified: { type: Boolean, default: false },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String },
     salt: { type: String },
     phone: { type: String },
@@ -22,37 +23,47 @@ const userSchema = new mongoose.Schema({
     summary: { type: String },
     skills: [{ type: String }],
     careerGoals: { type: String },
-    address: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }],
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
     socialMedia: {
-        twitter: { type: String },
-        linkedin: { type: String },
-        facebook: { type: String },
+      twitter: { type: String },
+      linkedin: { type: String },
+      facebook: { type: String },
     },
-    
+
     // Professional Info
-    accomplishments: { type: mongoose.Schema.Types.ObjectId, ref: 'Accomplishment' },
-    education: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Education' }],
-    experience: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Experience' }],
-    recommendations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recommendation' }],
+    accomplishments: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Accomplishment",
+    },
+    education: [{ type: mongoose.Schema.Types.ObjectId, ref: "Education" }],
+    experience: [{ type: mongoose.Schema.Types.ObjectId, ref: "Experience" }],
+    recommendations: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Recommendation" },
+    ],
     resumeLink: { type: String },
     interests: {
-        companies: [{ type: String }],
-        topics: [{ type: String }],
+      companies: [{ type: String }],
+      topics: [{ type: String }],
     },
-}, {
+  },
+  {
     toJSON: {
-        transform(doc, ret) {
-            delete ret.__v;
-            delete ret.password;
-            delete ret.salt;
-            delete ret.verification_code;
-            delete ret.expiry;
-            delete ret.deleted;
-        }
+      transform(doc, ret) {
+        delete ret.__v;
+        delete ret.password;
+        delete ret.salt;
+        delete ret.verification_code;
+        delete ret.expiry;
+        delete ret.deleted;
+      },
     },
-    timestamps: true
-});
+    timestamps: true,
+  }
+);
 
-userSchema.index({ email: 1, deleted: 1, _id: 1 }, { unique: true, partialFilterExpression: { deleted: false } });
+userSchema.index(
+  { email: 1, deleted: 1, _id: 1 },
+  { unique: true, partialFilterExpression: { deleted: false } }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
