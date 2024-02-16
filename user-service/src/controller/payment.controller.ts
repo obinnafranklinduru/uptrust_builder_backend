4fe7724a365f0c364d18707473fa827fb8957858
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-02-16 12:57:47 -0800

    initial commit

--- user-service/src/controller/payment.controller.ts
+++
@@ -1,5 +0,0 @@
-import { container } from "tsyringe";
-import { Request, Response, NextFunction } from "express";
-import { PaymentService } from "../service/payment.service";
-
-const service = container.resolve(PaymentService);
