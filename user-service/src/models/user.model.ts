f8b87024f0ddcf025acc408c656cd7d33a5470b4
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-02-18 13:43:33 -0800

    Update user.model.ts

--- user-service/src/models/user.model.ts
+++ user-service/src/models/user.model.ts
@@ -4,7 +4,7 @@ import { IUserModel } from "./interfaces/user.interface";
 const userSchema: Schema = new Schema(
   {
     phone: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     salt: { type: String, required: true },
     verification_code: { type: Number },
