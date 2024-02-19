e600760008e242307ec17e473b8abd0a622f7489
Myles181 <cyrile450@gmail.com>
2024-02-18 18:19:04 -0800

    move

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import os
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+
 # from flask_jwt import JWT
 import jwt
 
@@ -11,13 +12,10 @@ secret_key = os.getenv("SECRET_KEY")
 
 app.config["SECRET_KEY"] = secret_key
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
-
 app.config["UOLOAD_FOLDER"] = os.getcwd()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///upthrust.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MONGO_URI'] = 'mongodb+srv://uptrustdatabase:MA5SxJuiWIBf1Mef@dictionary.dxa2nfr.mongodb.net/uptrustdatabase?retryWrites=true&w=majority'
 
-
-db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 def verify_token():
     token = request.headers.get('Authorization').split(" ")[1]
@@ -41,21 +39,31 @@ def verify_token():
 
 """ MODEL """
 
-class Email(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255*50), nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    applicant_name = db.Column(db.String(100), nullable=False)
-    applicant_email = db.Column(db.String(100), nullable=False)
-    job_description = db.Column(db.Text, nullable=False)
+from datetime import datetime
+# from bson.objectid import ObjectId
 
-class CVFile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    file_name = db.Column(db.String(100))
-    file_data = db.Column(db.LargeBinary)
-    score = db.Column(db.Integer)
-    email_address = db.Column(db.String(100), nullable=False)
+class Email:
+    def __init__(self, email_gen, user_id, user_name, email_address, job_description):
+        self.email = email_gen
+        self.user_id = user_id
+        self.applicant_name = user_name
+        self.applicant_email = email_address
+        self.job_description = job_description
+        self.date_time = datetime.now()
 
-with app.app_context():
-    db.create_all()
+class CVFile:
+    def __init__(self, user_id, file_name, file_data, score, email_address):
+        self.user_id = user_id
+        self.file_name = file_name
+        self.file_data = file_data
+        self.score = score
+        self.email_address = email_address
+        self.date_time = datetime.now()
+
+class Jobs:
+    def __init__(self, file_name, file_data):
+        self.file_name = file_name
+        self.file_data = file_data
+        self.date_time = datetime.now()
+
+
